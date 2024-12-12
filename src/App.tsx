bfff98cab609be8ea44cc4dca3196e463df309c7
AndrePacolla <andreluizzpacolla@outlook.com>
2024-12-12 15:30:32 -0300

    estrutuando styled component no App

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import * as C from "./App.styles";
+
+
+const  App = () => {
+
+  return (
+    
+
+      <C.Container>
+        <C.Header>
+          <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        </C.Header>
+      </C.Container>
+      
+    
+    
+    
+     
+    
+  )
+}
+
+export default App;
