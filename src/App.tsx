82dd09340cb502180101e6acfe2610390d9adf5a
AndrePacolla <andreluizzpacolla@outlook.com>
2024-12-14 18:16:59 -0300

    create dateFilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import {Item} from "./types/Item";
+import {Category} from "./types/Category";
+import {items} from "./data/items";
+import {categories} from "./data/categories";
+
+
 
 
 const  App = () => {
 
-  return (
-    
 
+  const [list, setLIst] = useState<Item[]>(items);
+  
+  const [currentMonth, setCurrentMonth] = useState();
+
+  return (
       <C.Container>
         <C.Header>
           <C.HeaderText>Sistema Financeiro</C.HeaderText>
