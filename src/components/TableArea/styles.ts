7074970e622b5b88a9c2cf39e973b4f1bd4047aa
AndrePacolla <andreluizzpacolla@outlook.com>
2024-12-22 17:17:00 -0300

    TableItem

--- src/components/TableArea/styles.ts
+++ src/components/TableArea/styles.ts
@@ -7,14 +7,15 @@ export const Table = styled.table`
         border: 2px solid ;
         border-radius: 10px;
         margin-top: 20px;
+        font-size: 20px;
+        font-family: 'Times New Roman', Times, serif;
         background-color:rgba(250, 237, 57, 0.67) ;
 
 `;
 
 
-export const TableHeadColumn = styled.th<{width?: number}>`
-        width: ${props => props.width ? `${ props.width}px` : "auto"};
+export const TableHeadColumn = styled.th <{width?: number}>`
+        width: ${props => props.width ? `${props.width}px` : "auto"};
         padding:10px 0;
         text-align: left;
-
 `;
--- src/data/items.ts
+++ src/data/items.ts
@@ -4,6 +4,6 @@ import {Item} from "../types/Item"
 export const items: Item[]  = [
     {date: new Date(2024, 11 ,17), category: "food", title: "DevsBurger", value: 32.12},
     {date: new Date(2024, 11 ,17),category:"food", title: "MacDonals", value: 28},
-    {date: new Date(2024, 11 ,17), category: "rent", title: "Aluguel Apt", value: 2300},
-    {date: new Date(2024, 11 ,16), category: "salary", title: "Salário Dvs", value: 4500}
+    {date: new Date(2024, 11 ,17), category: "rent", title: "Aluguel Apt", value: 2.300},
+    {date: new Date(2024, 11 ,16), category: "salary", title: "Salário Dvs", value: 3.500}
 ]
