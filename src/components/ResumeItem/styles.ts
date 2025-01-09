15c10022733662c19daad31c3270301fc88388c0
AndrePacolla <andreluizzpacolla@outlook.com>
2025-01-08 21:32:38 -0300

    setExpense and seIncome

--- src/components/ResumeItem/styles.ts
+++ src/components/ResumeItem/styles.ts
@@ -8,11 +8,14 @@ export const Container = styled.div`
 export const Title = styled.div`
     font-size: 20px;
     font-weight: bold;
+    color:gray;
     text-align: center;
 
 `;
 
 export const Info = styled.div`
     text-align: center;
+    font-weight: bold;
+    color: black;
     margin-top: 15px;
 `;
--- src/data/items.ts
+++ src/data/items.ts
@@ -2,12 +2,12 @@ import {Item} from "../types/Item"
 
 
 export const items: Item[]  = [
-    {date: new Date(2024, 10 ,6), category: "food", title: "Devs Burger", value: 32.12},
-    {date: new Date(2024, 10 ,9), category: "salary", title: "Salário front", value: 3.512},
-    {date: new Date(2024, 11 ,22), category: "food", title: "Devs Burger", value: 32.12},
+    {date: new Date(2024, 10 ,6), category: "food", title: "Devs Burger", value: 3212},
+    {date: new Date(2024, 10 ,9), category: "salary", title: "Salário front", value: 3512},
+    {date: new Date(2024, 11 ,22), category: "food", title: "Devs Burger", value: 3212},
     {date: new Date(2024, 11 ,27), category: "salary", title: "Devs", value: 3512},
 
-    {date: new Date(2024, 11 ,3),category:"food", title: "MacDonals", value: 68.99},
+    {date: new Date(2024, 11 ,3),category:"food", title: "MacDonals", value: 6899},
     {date: new Date(2024, 11 ,17), category: "rent", title: "Aluguel Apt", value: 2300},
     {date: new Date(2024, 11 ,17), category: "rent", title: "Viagem Praia", value: 870},
     {date: new Date(2025, 0 ,17), category: "salary", title: "Salário front", value: 4300},
